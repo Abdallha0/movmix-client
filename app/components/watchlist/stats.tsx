ee0ff327b884fab69dfc525a82f30c2f5d59a0dd
Abdallha0 <164950184+Abdallha0@users.noreply.github.com>
2025-12-08 14:14:39 +0200

    Update stats.tsx

--- app/components/watchlist/stats.tsx
+++ app/components/watchlist/stats.tsx
@@ -1,35 +1,36 @@
 import { CheckCircle, Clock, Eye, Film } from "lucide-react";
+import styles from "./css/stats.module.css";
 
 export function Stats(){
     return(
-                <div className="stats-section">
-          <div className="stat-card">
-            <div className="stat-icon gold">
+                <div className={styles.stats_section}>
+          <div className={styles.stat_card}>
+            <div className={`${styles.stat_icon} ${styles.gold}`}>
               <Film size={22} />
             </div>
-            <div className="stat-value">8</div>
-            <div className="stat-label">Total Movies</div>
+            <div className={styles.stat_value}>8</div>
+            <div className={styles.stat_label}>Total Movies</div>
           </div>
-          <div className="stat-card">
-            <div className="stat-icon blue">
+          <div className={styles.stat_card}>
+            <div className={`${styles.stat_icon} ${styles.blue}`}>
               <Eye size={22} />
             </div>
-            <div className="stat-value">2</div>
-            <div className="stat-label">Currently Watching</div>
+            <div className={styles.stat_value}>2</div>
+            <div className={styles.stat_label}>Currently Watching</div>
           </div>
-          <div className="stat-card">
-            <div className="stat-icon green">
+          <div className={styles.stat_card}>
+            <div className={`${styles.stat_icon} ${styles.green}`}>
               <CheckCircle size={22} />
             </div>
-            <div className="stat-value">3</div>
-            <div className="stat-label">Completed</div>
+            <div className={styles.stat_value}>3</div>
+            <div className={styles.stat_label}>Completed</div>
           </div>
-          <div className="stat-card">
-            <div className="stat-icon red">
+          <div className={styles.stat_card}>
+            <div className={`${styles.stat_icon} ${styles.red}`}>
               <Clock size={22} />
             </div>
-            <div className="stat-value">3</div>
-            <div className="stat-label">Plan to Watch</div>
+            <div className={styles.stat_value}>3</div>
+            <div className={styles.stat_label}>Plan to Watch</div>
           </div>
         </div>
     )
