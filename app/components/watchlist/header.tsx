9571dff94c3766266eec72a6a93a2dfe4ae11642
Abdallha0 <164950184+Abdallha0@users.noreply.github.com>
2025-12-08 13:14:35 +0200

    Update header.tsx

--- app/components/watchlist/header.tsx
+++ app/components/watchlist/header.tsx
@@ -1,12 +1,12 @@
 import { Search } from "lucide-react";
-
+import styles from "./css/header.module.css"
 export function Header() {
     return (
-        <header className="watchlist_header">
-            <div className="header_content">
-                <div className="search_container">
-                    <Search className="search_icon" size={18} />
-                    <input type="text" className="search_input" placeholder="Search your watchlist..." />
+        <header className={styles.watchlist_header}>
+            <div className={styles.header_content}>
+                <div className={styles.search_container}>
+                    <Search className={styles.search_icon} size={18} />
+                    <input type="text" className={styles.search_input} placeholder="Search your watchlist..." />
                 </div>
             </div>
         </header>
